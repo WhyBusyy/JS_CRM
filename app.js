0eb21efa862fc02f26b31fdc465fca0d5cb0a742
WhyBusyy <ybg6152@naver.com>
2023-12-05 20:09:27 +0900

    CRM 2일차 기본 미션 완료 - item 상세페이지 // 아이템 월간 매출

--- app.js
+++ app.js
@@ -368,13 +368,33 @@ app.get("/item", (req, res) => {
 });
 
 app.get("/item/:ID", (req, res) => {
-  const itemData = [];
+  let itemData = [];
+  let itemtotalData = [];
   let itemID = req.params.ID;
   itemData.push(itemResults.find((item) => item.id === itemID));
 
-  res.render("item_detail", {
-    header: itemHdResults,
-    data: itemData,
+  const dataQuery = `SELECT
+  strftime('%Y-%m', orders.ordered_at) AS month,
+  SUM(item.unit_price) AS total,
+  COUNT(Orderitem.id) AS count
+  FROM item
+  JOIN orderitem ON item.id = orderitem.item_id
+  JOIN orders ON orderitem.order_id = orders.id
+  WHERE item.id = ?
+  GROUP BY  month
+  ORDER BY month ASC;`;
+
+  db.all(dataQuery, [itemID], (err, rows) => {
+    if (err) {
+      console.error(err.message);
+    } else {
+      itemtotalData = rows;
+      res.render("item_detail", {
+        header: itemHdResults,
+        data: itemData,
+        itemtotalData: itemtotalData
+      });
+    }
   });
 });
 
